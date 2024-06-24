7614e11b5a9d6a586f972ad89b67c87768674d5e
junaidjesan2 <junaidjesan2@gmail.com>
2024-06-24 19:51:04 +0600

    insert a tail learned and solved problem

---
+++ week-02/singly linked list operations/insert_at-tail.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int val;
+    Node *next;
+
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+    }
+};
+void insert_at_tail(Node *&head, int v)
+{
+    Node *newNode = new Node(v);
+    if (head == NULL)
+    {
+        head = newNode;
+        return;
+    }
+
+    Node *tmp = head;
+    while (tmp->next != NULL)
+    {
+        tmp = tmp->next;
+    }
+    tmp->next = newNode;
+}
+void print_linked_list(Node *head)
+{
+    cout << endl;
+    cout << "Your linked List: ";
+    Node *tmp = head;
+    while (tmp != NULL)
+    {
+        cout << tmp->val << " ";
+        tmp = tmp->next;
+    }
+}
+int main()
+{
+    Node *head = NULL;
+    while (true)
+    {
+        cout << endl;
+        cout << "option 1: Insert at Tail" << endl;
+        cout << "option 2: Give a value" << endl;
+        cout << "option 3: break the list" << endl;
+        int op;
+        cin >> op;
+        if (op == 1)
+        {
+            int v;
+            cout << "Insert a value : ";
+            cin >> v;
+            insert_at_tail(head, v);
+        }
+        else if (op == 2)
+        {
+            print_linked_list(head);
+        }
+        else if (op == 3)
+        {
+            break;
+        }
+    }
+
+    return 0;
+}
